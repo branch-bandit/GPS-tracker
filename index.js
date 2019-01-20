bbcca3aa7adfa6cbc6c6f6d6144c70d99d235823
Artur Radomyski <widzepsaprzeddrzwiami@gmail.com>
2019-01-21 00:05:28 +0100

    Simplified time calculation - got rid of geolocation timestamps, now only using simple timer

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const minDistance = 0.000000000001;
-let timestamps;
 let recordedCoords;
 let isTracking;
 let secondsCounted;	
@@ -7,7 +6,6 @@ let trackLocation;
 let incrementTimer;
 
 const initializeState = () => {
-	timestamps = [];
 	recordedCoords = [];
 	isTracking = false;
 	secondsCounted = 0;	
@@ -42,7 +40,6 @@ const calculateDistance = (lat1, lon1, lat2, lon2) => {
 
 const writeCoords = (position) => {
 	// Records geolocation coordinates without checking for movement. Used only for first and last coordinates.
-	timestamps.push(position.timestamp);
 	recordedCoords.push({
 		latitude: position.coords.latitude,
 		longitude: position.coords.longitude
@@ -84,35 +81,16 @@ const onGeoSuccess = (position) => {
 }
 
 
-const calculateAndShowTime = (timeArr) => {
-	// Returns final trip time from first and last geolocation timestamp
-	let tripTime = timeArr[timeArr.length -1] - timeArr[0];
-	let	parsedTime = msToTime(tripTime);
-	document.getElementById("time-results").innerHTML = parsedTime;
-} 
-
-
-const msToTime = (duration) => {
-	let  seconds = parseInt((duration / 1000) % 60),
-	minutes = parseInt((duration / (1000 * 60)) % 60),
-	hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-
-	hours = (hours < 10) ? "0" + hours : hours;
-	minutes = (minutes < 10) ? "0" + minutes : minutes;
-	seconds = (seconds < 10) ? "0" + seconds : seconds;
-
-	return hours + ":" + minutes + ":" + seconds; 
-}
-
-
 const startTimer = () => {
-	// This timer is only shown while ride is in progress. The calculateAndShowTime() function is used for calculating final time value.
+	// Note: this timer doesn't require a GPS or even data connection to work.
+	// So tracking witout a working connection will only track the time.
 	let incrementTimer = (val) => {
 		return val > 9 ? val : "0" + val; 
 	}
 
 	let updateTimer = () => {
-	    document.getElementById("time-results").innerHTML = incrementTimer(parseInt(secondsCounted/3600,10)) + ":" + incrementTimer(parseInt(secondsCounted/60,10)) + ":" + incrementTimer(++secondsCounted%60);
+	    document.getElementById("time-results").innerHTML = incrementTimer(parseInt(secondsCounted/3600,10)) + ":" + incrementTimer(parseInt(secondsCo
+	    	ted/60,10)) + ":" + incrementTimer(++secondsCounted%60);
   	}
 
 	countTime = setInterval(updateTimer, 1000);
@@ -159,7 +137,6 @@ const startTracking = () => {
 const stopTrackingAndReset = () => {
 	getLocationWithPromise().then((position) => {
 		writeCoords(position);
-		calculateAndShowTime(timestamps);
 		processAndShowDistance();
 
 		stopTimer();
